4b0053ac0b5bb63246a38630549085fef6b46a7d
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2023-02-04 21:20:16 +0530

    Update Stickers_v2.py

--- userbot/plugins/Stickers_v2.py
+++ userbot/plugins/Stickers_v2.py
@@ -1,5 +1,5 @@
 # Stickers Pluging V2
-# @TechnoAyanOfficial
+# @Thealphabotz
 
 """Available Commands:
 .kang [Optional Emoji]
@@ -33,7 +33,7 @@ from userbot import ALIVE_NAME
 from userbot.exclusive import STICKER_PACK_NAME, ANIMATED_STICKER_PNAME
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No Name Set Yet, Check @TechnoAyanOfficial"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No Name Set Yet, Check @TheAlphabotz"
 
 @borg.on(admin_cmd(pattern="keng ?(.*)"))
 async def _(event):
@@ -54,7 +54,7 @@ async def _(event):
     packshortname = f"PremiumStickers_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
-    file_ext_ns_ion = "@TechnoAyanOfficial_Sticker.png"
+    file_ext_ns_ion = "@TheAlphabotz_Sticker.png"
     file = await borg.download_file(reply_message.media)
     uploaded_sticker = None
     if is_a_s:
@@ -62,7 +62,7 @@ async def _(event):
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{ANIMATED_STICKER_PNAME}"
         if userid == 1111847352:
-            packshortname = "TechnoAyanOfficial_animated"
+            packshortname = "TheAlphaBotz_animated"
         else:
             packshortname = f"PremiumStickers_Animated_{userid}" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
