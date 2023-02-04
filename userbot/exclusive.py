aafbbfe507c0c654c0eb16eccf2ccd67e43a4dfa
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2023-02-04 21:39:18 +0530

    Update exclusive.py

--- userbot/exclusive.py
+++ userbot/exclusive.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020 by TechnoAyanOfficial@Github, < https://github.com/TechnoAyanOfficial >.
+# Copyright (C) 2020 by utkarshdubey2008@Github, < https://github.com/utkarshdubey2008 >.
 #
-# This file is part of < https://github.com/TechnoAyanOfficial/TechnoAyanBOT > project,
+# This file is part of < https://github.com/utkarshdubey2008/AlphaXuserbot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TechnoAyanOfficial/TechnoAyanBOT/blob/Exclusive/LICENSE >
+# Please see < https://github.com/utkarshdubey2008/AlphaXuserbot/blob/Exclusive/LICENSE >
 #
 # All rights reserved.
 
@@ -14,6 +14,6 @@ ANIMATED_STICKER_PNAME = os.environ.get("ANIMATED_STICKER_PNAME", "Creepy Animat
 
 AUTONAME = os.environ.get("AUTONAME", None)
 
-CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/TechnoAyanBoT/80")
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/thealphabotz/1")
 
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
