e4ce81f2f1173446e1bee7055debc1c49b3ad65f
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2023-02-06 21:00:08 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -11,8 +11,8 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(os.environ.get("API_ID", None))
+API_HASH = os.environ.get("API_HASH", None)
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
